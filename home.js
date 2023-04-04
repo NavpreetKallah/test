3ab7995eb91766215e55e5688063e2169cc7d0da
navpreetkallah <navpreetkallah@gmail.com>
2023-04-04 21:57:08 +0100

    made a new home page

---
+++ home.js
@@ -0,0 +1,9 @@
+document.getElementById("course").addEventListener("click", function () {
+    window.open("https://www.theodinproject.com/paths/foundations/courses/foundations", "_blank")
+  });
+  document.getElementById("website").addEventListener("click", function () {
+    window.open("https://navpreetkallah.github.io/test/project%201.html", "_blank")
+  });
+  document.getElementById("calculator").addEventListener("click", function () {
+    window.open("https://navpreetkallah.github.io/test/calculator.html", "_blank")
+  });
