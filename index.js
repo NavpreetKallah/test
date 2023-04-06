ce004a60b183b0a98963cad0a73cc138927615f4
navpreetkallah <navpreetkallah@gmail.com>
2023-04-06 22:34:26 +0100

    added etch a sketch

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 document.getElementById("course").addEventListener("click", function () {
     window.open("https://www.theodinproject.com/paths/foundations/courses/foundations", "_blank")
-  });
-  document.getElementById("website").addEventListener("click", function () {
+});
+document.getElementById("website").addEventListener("click", function () {
     window.open("https://navpreetkallah.github.io/test/Projects/Website/website.html", "_blank")
-  });
-  document.getElementById("calculator").addEventListener("click", function () {
+});
+document.getElementById("calculator").addEventListener("click", function () {
     window.open("https://navpreetkallah.github.io/test/Projects/Calculator/calculator.html", "_blank")
-  });
-  document.getElementById("RPS").addEventListener("click", function () {
+});
+document.getElementById("RPS").addEventListener("click", function () {
     window.open("https://navpreetkallah.github.io/test/Projects/Rock%20Paper%20Scissors/RPS.html", "_blank")
-  });
+});
+document.getElementById("EAS").addEventListener("click", function () {
+  window.open("https://navpreetkallah.github.io/test/Projects/Etch%20A%20Sketch/EAS.html", "_blank")
+});
