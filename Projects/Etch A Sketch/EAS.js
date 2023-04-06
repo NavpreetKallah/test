a3f693123d6f91fcf62f1800faae2c67bd751866
navpreetkallah <navpreetkallah@gmail.com>
2023-04-06 23:11:51 +0100

    didnt code the grid change selection propperly

--- Projects/Etch A Sketch/EAS.js
+++ Projects/Etch A Sketch/EAS.js
@@ -52,7 +52,7 @@ panels.forEach((panel) => {
 })
 document.getElementById("change").addEventListener("click", function () {
   size = (prompt("Please enter how many panels wide you would like the grid to be between 1-100")-1)
-  if (100 >=size >=1) {
+  if (100 >=size && size>=1) {
   document.getElementById("container").innerHTML = "";
   update()
   detect_hover()
