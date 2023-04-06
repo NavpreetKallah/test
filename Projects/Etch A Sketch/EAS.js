f2c2900da510fd2f8f23a79b803339626c81b23c
navpreetkallah <navpreetkallah@gmail.com>
2023-04-06 23:07:41 +0100

    Added a change grid feature

--- Projects/Etch A Sketch/EAS.js
+++ Projects/Etch A Sketch/EAS.js
@@ -3,6 +3,7 @@ const row = document.createElement("row");
 let multiplyer = 0;
 let important = 0;
 let size = 16;
+function update() {
 for (let i = 0;i <= size;i++) {
   var div = document.createElement('div');
   div.textContent = ""
@@ -23,25 +24,40 @@ for (let i = 0;i <= size;i++) {
   important = 1;
   multiplyer = multiplyer + 1
 }
-//
-const panels = document.querySelectorAll('div.panel');
+width = screen.width
+panel_size = Math.round((((width*0.66) / size)/4))
+panel_size = panel_size.toString()
+panel_size = panel_size.concat("px")
+const panel_list = document.querySelectorAll("div.panel");
+panel_list.forEach((temp) => {
+temp.style.padding = panel_size;
+  })
+}
+update()
+function detect_hover() {
+panels = document.querySelectorAll('div.panel');
 panels.forEach((panel) => {
 
   panel.addEventListener("mouseenter", () => {
     panel.classList.add("black");
   });
 });
-let width = screen.width;
-panel_size = Math.round(((width/size)-10)/4)
-panel_size = panel_size.toString()
-panel_size = panel_size.concat("px")
-const panel_list = document.querySelectorAll("div.panel");
-panel_list.forEach((panel) => {
-  panel.style.padding = panel_size;
-})
+}
+detect_hover()
 
 document.getElementById("clear").addEventListener("click",function() {
 panels.forEach((panel) => {
     panel.classList.remove("black");
 });
 })
+document.getElementById("change").addEventListener("click", function () {
+  size = (prompt("Please enter how many panels wide you would like the grid to be between 1-100")-1)
+  if (100 >=size >=1) {
+  document.getElementById("container").innerHTML = "";
+  update()
+  detect_hover()
+  }
+  else {
+    size = 16;
+  }
+});
